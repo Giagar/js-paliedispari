0817f0ed1d9341f88e5c6288782a2f17acb89105
Gianpaolo <pao.barga@gmail.com>
2021-01-07 15:51:51 +0100

    commit iniziale: fatto esercizio sui palindromi

---
+++ js/script.js
@@ -0,0 +1,18 @@
+//esercizio: palindromi
+// var word = prompt("Digitare una parola");
+
+// function palindromi(word) {
+//     console.log(word);
+//     var test = word.split("").reverse().join("");
+//     if(word === test) {
+//         console.log("La parola inserita è palindroma.");
+//     } else {
+//         console.log("La parola inserita non è palindroma.");
+//     }        
+// }
+
+// palindromi(word);
+
+//esercizio: pari e dispari
+
+
