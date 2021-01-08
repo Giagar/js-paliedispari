61b64a1e659d4073faff2c807e410a1cffb939c7
Gianpaolo <pao.barga@gmail.com>
2021-01-08 11:35:55 +0100

    correzione esercizio

--- js/script.js
+++ js/script.js
@@ -18,6 +18,20 @@ function palindromi(word) {
 
 palindromi(word);
 
+//versione base: crea una funzione con for da usare invece di split, reverse,
+/*function reverse(word) {
+    var arr = [];
+    word = word.toLowerCase();
+
+    for(var i = 0; i < word.length; i++) {
+        arr.unshift(word[i]);
+    }
+
+    return arr.join("");       
+}
+*/
+
+
 // ------------------------
 
 //esercizio: pari e dispari
@@ -31,7 +45,7 @@ function randomNumber(min, max) {
 }
 function evenNum(num) {
     return num%2 === 0;
-}
+}                   
 
 //testing user input
 if(!/pari|dispari/i.test(userSide)) {
@@ -47,7 +61,7 @@ if(!/pari|dispari/i.test(userSide)) {
         computerNumber = randomNumber(1, 5);
         var sum = computerNumber + userNumber;
 
-        // check if the user's guess is right
+        // check if the user's guess is correct
         if( (evenNum(sum) && /^pari$/i.test(userSide)) || (!evenNum(sum) && !/^pari$/i.test(userSide)) ) {
             alert(`Hai scelto ${userSide} e il numero ${userNumber}, il computer ha scelto il numero ${computerNumber}. La somma è ${sum}: hai vinto!` )
         } else {
@@ -55,4 +69,37 @@ if(!/pari|dispari/i.test(userSide)) {
         }
     }
 }
+
+/* 
+//Soluzione migliore (by Domenico G.)
+//semplificazione anche perché mancano i checking points
+// chiedo all'utente un numero da 1 a 5, e di scegliere pario o dispari
+var evenOddUser = prompt('Scegli fra pari o dispari');
+var userNumb = parseInt(prompt('Inserisci un numero da 1 a 5'));
+
+// creo una funzione per generare un numero
+function random(min, max){
+  return Math.floor(Math.random() * (max + 1 - min) + min);
+}
+
+// sommo i due numeri
+var somma = userNumb + random(1,5);
+
+// creo una funzione per stabilire se il numero è pari o dispari
+function evenOdd(somma){
+  if (somma % 2 === 0) {
+    return 'pari';
+  } else {
+    return 'dispari';
+  }
+}
+
+// confronto i risultati e indico chi ha vinto
+if (evenOddUser == evenOdd(somma)) {
+  alert('hai vinto')
+} else {
+  alert('Hai perso')
+
+
+*/
 
